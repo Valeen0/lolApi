3433748b7e02369a8f01ccd31fd757ef3f13277e
Valentin Viz <emilio66656@gmail.com>
2022-12-28 12:38:37 -0300

    deleted useData

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
-import { useData } from '../hooks/useData';
 import './styles/buscador.css';
-
-const apiKey ="RGAPI-3c486d65-3f5b-4c7b-b653-98a6a0c62560";
-const protocol = "https://";
-const region = "la2.api.riotgames.com";
-const summEndPoint = "/lol/summoner/v4/summoners/by-name/"
 
 export function Buscador() {
 
     const [name, setName] = useState();
-
-    const data = useData(`${protocol}${region}${summEndPoint}${name}?ap i_key=${apiKey}`)
-
 
     const getName = ev =>{
         ev.preventDefault();
@@ -25,8 +16,8 @@ export function Buscador() {
     }
 
     useEffect(()=>{
+
         
-        console.log(data)
 
     },[name])
     
