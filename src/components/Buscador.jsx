0360a014bfab66b2fb220eb09f9c4d345e0724d5
Yanzz <yonielkpo@gmail.com>
2022-12-28 15:33:37 -0300

    bug

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -1,19 +1,9 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
-<<<<<<< HEAD
-import { useData } from '../hooks/useData';
-
-const protocol = "https://";
-const region = "la2.api.riotgames.com";
-const summEndPoint = "/lol/summoner/v4/summoners/by-name/"
-
-export function Buscador() {
-=======
 import { getFetch } from '../services/getFetch';
 
 // import meta.env.VITE_API_KEY;
->>>>>>> master
 
 export function Buscador({setState}) {
     
@@ -22,18 +12,6 @@ export function Buscador({setState}) {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
-<<<<<<< HEAD
-    const data = useData(`${protocol}${region}${summEndPoint}${name}?api_key=${import.meta.env.VITE_API_KEY}`)
-
-
-    const getName = ev =>{
-        ev.preventDefault();
-        const name = ev.target.player.value;
-
-        setName(name);
-    }
-
-=======
     const url = `https://la2.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}?api_key=${import.meta.env.VITE_API_KEY}`;
 
     const getName = e =>{
@@ -57,11 +35,10 @@ export function Buscador({setState}) {
     },[name]);
     
 
->>>>>>> master
     return(
         <>
             <form onSubmit={getName}>
-                <input className='' type="text" autoComplete='off' name='player' placeholder='Buscar jugador'/>
+                <input className='input_text' type="text" autoComplete='off' name='player' placeholder='Buscar jugador'/>
                 <input type="submit" value="Buscar" />
             </form>
         </>
